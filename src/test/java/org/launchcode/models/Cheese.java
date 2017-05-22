d0efb355dc66fb8a4c0ec1ccab17fb19a4af05ae
Steven Ray Wise II <srwii2702@gmail.com>
2017-05-22 15:49:21 -0500

    added Cheese.java

---
+++ src/test/java/org/launchcode/models/Cheese.java
@@ -0,0 +1,30 @@
+package org.launchcode.models;
+
+/**
+ * Created by Steven on 5/21/2017.
+ */
+public class Cheese {
+    private String name;
+    private String description;
+    public String getName() {
+
+        return name;
+    }
+    private void setName(String aName) {
+
+        name = aName;
+    }
+    public String getDescription() {
+
+        return description;
+    }
+    private void setDescription(String aDescription) {
+
+        description = aDescription;
+    }
+    public static void main(String[] args) {
+
+    }
+
+
+}
